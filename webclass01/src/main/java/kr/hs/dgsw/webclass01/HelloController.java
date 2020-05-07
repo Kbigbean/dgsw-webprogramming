16a068661c2aa1e1a096ec98d488d8dc697f1de7
김태빈 <48699529+Kbigbean@users.noreply.github.com>
2020-05-07 14:08:18 +0900

    4/21

--- webclass01/src/main/java/kr/hs/dgsw/webclass01/HelloController.java
+++ webclass01/src/main/java/kr/hs/dgsw/webclass01/HelloController.java
@@ -1,12 +1,14 @@
 package kr.hs.dgsw.webclass01;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping
-    public String hello() {
-        return "Hello world";
-    } 
+    @GetMapping("/calc/{num1}/{calc}/{num2}/{val}")
+    public String calc(@PathVariable int num1, @PathVariable String calc, @PathVariable int num2, @PathVariable String val) {
+        return val;
+    }
 }
